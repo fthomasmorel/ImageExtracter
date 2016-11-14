0589b1576eed5cd518a6b817419913404a31ce68
fthomasmorel <florent@thomasmorel.io>
2016-11-14 15:48:18 +0100

    some fix

--- extractor.py
+++ extractor.py
@@ -19,7 +19,7 @@ from matplotlib import pyplot as plt
 #                                                      #
 ########################################################
 
-OUTPUT_DIR = './output/'
+OUTPUT_DIR = './output2/'
 BANK_IMG_FOLDER = './bank/'
 TEMPLATES_IMG_FOLDER = './picto/'
 
@@ -81,20 +81,17 @@ def getTemplates(path):
 
 def recognizePictogram(pictoZone, ratio):
     templates = getTemplates(TEMPLATES_IMG_FOLDER)
-    pattern = "-"
-    for template, tmpl_img in templates.items():
-        w, h = tmpl_img.shape[::-1]
-        tmpl_img = cv2.resize(tmpl_img, (int(round(ratio * w)), int(round(ratio * h))))
-        res = cv2.matchTemplate(pictoZone, tmpl_img, cv2.TM_CCOEFF_NORMED)
-        threshold = 0.5
-        loc = np.where(res >= threshold)
-        for pt in zip(*loc[::-1]):
-            pattern = template
-            break
-        if pattern != "-":
-            break
-    return pattern
+    for threashold in range(95, 50, -5):
+        threashold = float(threashold)/100.0
+        for template, tmpl_img in templates.items():
+            w, h = tmpl_img.shape[::-1]
+            tmpl_img = cv2.resize(tmpl_img, (int(round(ratio * w)), int(round(ratio * h))))
+            res = cv2.matchTemplate(pictoZone, tmpl_img, cv2.TM_CCOEFF_NORMED)
+            loc = np.where(res >= threashold)
+            for pt in zip(*loc[::-1]):
+                return template
 
+    return "-"
 
 def pictogramZones(coords):
     x = min(set(map((lambda (x, _): x), coords)))
@@ -143,9 +140,10 @@ def processFile(file, threashold = 0.63):
     shapes = classifyShapes(img, shapes, ratio)
     errors = 0
     extracted = 0
-    if "-" in shapes.keys():
-        errors = len(shapes["-"])
     for key in shapes.keys():
+        if key == "-":
+            errors = len(shapes["-"])
+            next
         extracted += len(shapes[key])
     missing = NB_PICTOS - (extracted + errors)
     return shapes, extracted, errors, missing
@@ -161,19 +159,19 @@ def writeImage(name, img, index):
     for dirname, _, _ in os.walk(OUTPUT_DIR):
         w, h = img.shape[::-1]
         img = img[20:h-20,20:w-20]
-        cv2.imwrite(dirname + name + "_" + str(index) + ".png", img)
+        #cv2.imwrite(dirname + name + "_" + str(index) + ".png", img)
         _, img = cv2.threshold(img, 220, 255, cv2.THRESH_BINARY)
-        cv2.imwrite(dirname + name + "_" + str(index) + "_dark.png", img)
+        #cv2.imwrite(dirname + name + "_" + str(index) + "_dark.png", img)
+        cv2.imwrite(dirname + name + "_" + str(index) + ".png", img)
         break
 
 
-def exportImages(shapes, extracted):
-    i = 0
-    for shape_dict in shapes:
-        for key in shape_dict.keys():
-            if key == "-": next
-            for img in shape_dict[key]:
-                drawProgressBar(float(i)/float(extracted), 0)
+def exportImages(shapes, extracted, number):
+    i = 0 + number
+    for key in shapes.keys():
+        if key != "-":
+            for img in shapes[key]:
+                #drawProgressBar(float(i)/float(extracted), 0)
                 writeImage(key, img, i)
                 i += 1
 
@@ -223,18 +221,17 @@ for dirname, _, filenames in os.walk(BANK_IMG_FOLDER):
             errors += error
             missings += missing
         if extracted > 15:
+            exportImages(result, extracted, extracteds)
             extracteds += extracted
             done += NB_PICTOS
-            shapes.append(result)
-
-        #if progress > 5: break
+            #shapes.append(result)
+#        if progress > 5: break
 
     if done > 0 :
         drawProgressBar(1, ((float(errors) + float(missings)) / float(done)) * 100)
         print "\n"
-        print "Exporting " + str(extracteds) + " Images..."
-        exportImages(shapes, extracteds)
-        drawProgressBar(1, 0)
+        #print "Exporting " + str(extracteds) + " Images..."
+        #drawProgressBar(1, 0)
         print "\n"
         print done, " pictograms treated"
         print errors, " pictograms misrecognized"
